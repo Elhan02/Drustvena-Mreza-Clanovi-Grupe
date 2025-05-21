1400c977b5adf9e85f5647a3240c0355b6193031
Elhan002 <elhanhajrovic02@gmail.com>
2025-05-21 20:31:16 +0200

    feat: Dodata funkcionalnost za upravljanje grupama.

---
+++ DrustvenaMreza/Models/Group.cs
@@ -0,0 +1,18 @@
+﻿namespace DrustvenaMreza.Models
+{
+    public class Group
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateCreated { get; set; }
+
+        //TODO public List<User> 
+
+        public Group(int id, string name, DateTime dateCreated)
+        {
+            Id = id;
+            Name = name;
+            DateCreated = dateCreated;
+        }
+    }
+}
---
+++ DrustvenaMreza/Repositories/GroupRepository.cs
@@ -0,0 +1,44 @@
+﻿using DrustvenaMreza.Models;
+using System.Globalization;
+
+namespace DrustvenaMreza.Repositories
+{
+    public class GroupRepository
+    {
+        private const string filePath = "data/grupe.csv";
+        public static Dictionary<int, Group> Data;
+
+        public GroupRepository()
+        {
+            if (Data == null)
+            {
+                Load();
+            }
+        }
+
+        private void Load()
+        {
+            Data = new Dictionary<int, Group>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines)
+            {
+                string[] attributes = line.Split(',');
+                int id = int.Parse(attributes[0]);
+                string name = attributes[1];
+                DateTime dateCreated = DateTime.ParseExact(attributes[2], "yyyy-MM-dd", CultureInfo.InvariantCulture);
+                Group group = new Group(id, name, dateCreated);
+                Data.Add(id, group);
+            }
+        }
+
+        public void Save()
+        {
+            List<string> lines = new List<string>();
+            foreach (Group group in Data.Values)
+            {
+                lines.Add($"{group.Id},{group.Name},{group.DateCreated.ToString("yyyy-MM-dd")}");
+            }
+            File.WriteAllLines(filePath, lines);
+        }
+    }
+}
